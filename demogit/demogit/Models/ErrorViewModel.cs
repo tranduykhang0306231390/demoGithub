b92b6c9fc174f5c8e7c87b8395c32a995049b77d
Trần Duy Khang <0306231390@caothang.edu.vn>
2025-10-16 10:34:59 +0700

    Demo github

---
+++ demogit/demogit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace demogit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
